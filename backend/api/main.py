bf0c97afacce031dd3e230b65f85cfc0f638a36c
Kanishq Chandwani <n11412143@qut.edu.au>
2026-06-24 17:06:14 +1000

    fix CORS for production

--- backend/api/main.py
+++ backend/api/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["*"],
     allow_methods=["GET"],
     allow_headers=["*"],
 )
